86cfdb96293330fb9fede587b35f1ec073dc351a
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2022-02-24 13:33:07 -0600

    overloading concept demo in java

---
+++ FPP/FPP_Day4/OverloadingDemo.java
@@ -0,0 +1,24 @@
+package lesson4;
+
+/*
+ * @author Nirmal Silwal
+ * @contact nsilwal@miu.edu
+*/
+
+public class OverloadingDemo {
+
+	/*
+	 * same method with different signature
+	 * 
+	 * different signature - number of arguments, argument type change
+	 * 
+	 * overloading verified at compile time - eg of complie time polymorphism or
+	 * early binding
+	 * 
+	 * return type determined at runtime
+	 * 
+	 * variation difference after the method name
+	 * 
+	 * compiler verifies the signature of the method
+	 */
+}
