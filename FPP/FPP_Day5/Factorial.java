21595bb216059b4719ffd12fecd946fe7ecc2194
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2022-02-26 04:59:54 -0600

    find factorial using recursive approach

---
+++ FPP/FPP_Day5/Factorial.java
@@ -0,0 +1,20 @@
+package lesson5;
+
+/*
+ * @author Nirmal Silwal
+ * @contact nsilwal@miu.edu
+*/
+
+public class Factorial {
+
+	public static int fact(int n) {
+		if (n == 0 || n == 1)
+			return 1;
+
+		return n * fact(n - 1);
+	}
+
+	public static void main(String[] args) {
+		System.out.println(fact(5));
+	}
+}
