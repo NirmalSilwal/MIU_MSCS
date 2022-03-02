a7f99c93d14dabc3aa5f25242234876b6ae9f7ae
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2022-03-02 03:37:59 -0600

    test file for assignment 4

---
+++ FPP/FPP_Day6/testing.java
@@ -0,0 +1,15 @@
+package lesson7;
+
+/*
+ * @author Nirmal Silwal
+ * @contact nsilwal@miu.edu
+*/
+
+public class testing {
+	public static void main(String[] args) {
+		System.out.println("/\\");
+		System.out.println("\\/");		
+		System.out.println(":)");		
+		System.out.println("||");		
+	}
+}
