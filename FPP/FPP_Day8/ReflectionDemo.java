eec307dc09109601d084b36c862d53b66dfc844b
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2022-03-02 03:41:21 -0600

    some reflection rules

---
+++ FPP/FPP_Day8/ReflectionDemo.java
@@ -0,0 +1,24 @@
+package lesson8;
+
+/*
+ * @author Nirmal Silwal
+ * @contact nsilwal@miu.edu
+*/
+
+public class ReflectionDemo {
+
+	/*
+	 * you have to get the benefit of Object methods only by overriding.
+	 * 
+	 * 1. toString() - print the status of the object
+	 * 
+	 * 2. equals() - to compare two objects are equal
+	 * 
+	 * 3. hashCode() -
+	 * 
+	 * 4. clone() -
+	 * 
+	 * when you want to compare two objects are equals or not you must override the
+	 * equals() method from Object class
+	 */
+}
