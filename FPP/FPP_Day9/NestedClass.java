a099a04d7e8160e0c50da37988eacf590a6dedb2
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2022-03-03 06:21:28 -0600

    discussed inner / nested class, their 4 variations in Java

---
+++ FPP/FPP_Day9/NestedClass.java
@@ -0,0 +1,24 @@
+package lesson9;
+
+/*
+ * @author Nirmal Silwal
+ * @contact nsilwal@miu.edu
+*/
+
+public class NestedClass {
+
+	/*
+	 * applicable in inner class
+	 * 
+	 * types of nested class - member, static, local, anonymous
+	 * 
+	 * applications - 1. sort the collecitons using Coparator interface 2. in GUI
+	 * applications
+	 * 
+	 * static methods inside innner class cannot access non static instance fields,
+	 * it can only access the static members of the outer class
+	 * 
+	 * 
+	 * lambda - (argument) -> {Body};
+	 */
+}
