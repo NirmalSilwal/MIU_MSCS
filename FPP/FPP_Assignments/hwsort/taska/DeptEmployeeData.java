e4d22d34c06cebd050b65f0a5e55b37692425b29
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2022-03-08 04:14:47 -0600

    data generator class for problem A

---
+++ FPP/FPP_Assignments/hwsort/taska/DeptEmployeeData.java
@@ -0,0 +1,20 @@
+package hwsort.taska;
+import java.time.LocalDate;
+
+
+public class DeptEmployeeData {
+
+	public static DeptEmployee[] getDeptData() {
+		Professor p0 = new Professor("Joe",40000, LocalDate.of(1999,12,24));
+		Professor p1 = new Professor("Bob",50000,LocalDate.of(1979,1,2));
+		Professor p2 = new Professor("Anna",45000,LocalDate.of(2000,5,21));
+		
+		Secretary s0 = new Secretary("Joan",50000,LocalDate.of(1987,2,15),10);
+		Secretary s1 = new Secretary("Janet",40000,LocalDate.of(1995,4,22),15);
+		
+		DeptEmployee[]  department = {p0,p1,p2,s0,s1};
+		return department;
+
+	}
+
+}
