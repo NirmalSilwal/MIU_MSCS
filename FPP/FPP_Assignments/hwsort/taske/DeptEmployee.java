677358381acb0a3b66c85347ded8b854d84231db
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2022-03-08 04:20:32 -0600

    implementing problem 5 using Lambda expression

---
+++ FPP/FPP_Assignments/hwsort/taske/DeptEmployee.java
@@ -0,0 +1,36 @@
+package hwsort.taske;
+
+import java.time.LocalDate;
+import java.util.Date;
+public class DeptEmployee {
+	private String name;
+	private double salary;
+	private LocalDate hireDate;
+	
+	public DeptEmployee(String name, double salary, LocalDate hireDate){
+		this.name = name;
+		this.salary = salary;
+		this.hireDate = hireDate;
+	}
+	//default implementation
+	public double getSalary() {
+		return salary;
+	}
+
+	public LocalDate getHireDate() {
+		return hireDate;
+	}
+	@Override
+	public String toString() {
+		return "name=" + name + ", salary=" + salary + ", hireDate=" + hireDate;
+	}
+	public void setHireDate(LocalDate hireDate) {
+		this.hireDate = hireDate;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
