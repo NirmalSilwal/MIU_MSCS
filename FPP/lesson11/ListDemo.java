6ce5d3a1a6eff8035dd4f337df820cc9b6815374
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2022-03-11 06:41:00 -0600

    discusses array limitations

---
+++ FPP/lesson11/ListDemo.java
@@ -0,0 +1,25 @@
+package lesson11;
+
+/*
+ * @author Nirmal Silwal
+ * @contact nsilwal@miu.edu
+*/
+
+public class ListDemo {
+
+	public static void main(String[] args) {
+
+		int[] arr = new int[5]; // length 5 is fixed
+//		arr[6] = 10; // array index out of bounds
+
+		int[] arr1 = new int[10];
+
+		/*
+		 * Abstract Data Type
+		 * 
+		 * ADT - you can do abstraction using interface or abstract class
+		 * 
+		 * 
+		 */
+	}
+}
