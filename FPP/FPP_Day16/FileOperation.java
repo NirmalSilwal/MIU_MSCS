4606b2e48fb399ad53a2caa7f16250178ef11dcc
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2022-03-19 20:19:27 -0500

    various file handling operation in Java

---
+++ FPP/FPP_Day16/FileOperation.java
@@ -0,0 +1,22 @@
+package lesson16;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+/*
+ * @author Nirmal Silwal
+ * @contact nsilwal@miu.edu
+*/	
+
+public class FileOperation {
+
+	public static void main(String[] args) {
+		try {
+			FileReader reader = new FileReader("text.txt");
+			
+		} catch (IOException e) {
+
+		}
+	}
+
+}
